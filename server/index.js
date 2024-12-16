1b6b1aab0133d4632b9633a6da8496bdfbb6187c
Subham11258 <subhamguptaslg.1996@gmail.com>
2024-12-16 22:01:42 +0530

    created registration and login setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const userRoutes = require("./routes/userRoutes");
 const mongoose = require('mongoose');
 const dburl = "mongodb+srv://Subham11258:654finVE1I2XN79L@cluster-projects.ggb8z.mongodb.net/mybookings?retryWrites=true&w=majority&appName=Demo";
+app.use(express.json());
 
 mongoose.connect(dburl).then(()=>console.log("Connected to db")).catch((err)=>console.log(err));
-
+app.use('/',userRoutes);
 
 app.listen(3000,()=>{
     console.log('server is connected on port 3000')
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const User = require("../models/userModel");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+router.post('/register',async(req,res)=>{
+    try{
+        const userExist = await User.findOne({email:req.body.email});
+    if(userExist){
+        res.send({
+            success:false,
+            message:'user already exist',
+        })
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    req.body.password = hashedPassword;
+    const newUser = await new User(req.body);
+    await newUser.save();
+    res.send({
+        success:true,
+        message:"user created successfully",
+    })
+    }
+    catch(err){
+        console.log(err);
+        res.send({
+            success:false,
+            message:err,
+        })
+    }
+    
+});
+
+router.post('/login',async(req,res)=>{
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if(!user){
+            res.send({
+                success:false,
+                message:'user does not exist'
+            })
+        }
+        const validPassword = await bcrypt.compare(req.body.password,user.password);
+        if(!validPassword){
+            res.send({
+                success:false,
+                message:'Invalid password'
+            })
+        }
+        res.send({
+            success:true,
+            message:'logged in'
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+
+module.exports = router;
