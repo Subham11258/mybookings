669a8d310938a704656b0a9f553f4c8944409a4a
Subham11258 <subhamguptaslg.1996@gmail.com>
2025-01-05 12:11:44 +0530

    updated backend url

--- client/client/src/api/index.js
+++ client/client/src/api/index.js
@@ -1,7 +1,7 @@
  import axios from 'axios';
 
  export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://mybookings-backend-86qj.onrender.com',
     headers:{
         'Content-Type': 'application/json',
         'Authorization':`Bearer ${localStorage.getItem('token')}`,
